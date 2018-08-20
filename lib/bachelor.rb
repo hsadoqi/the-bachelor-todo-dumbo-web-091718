05a6ddbccfcf4cc3156704c17323a3115a6a1261
hsadoqi@gmail.com <hsadoqi@gmail.com>
2018-08-20 02:18:02 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,7 +2,7 @@ def get_first_name_of_season_winner(data, season)
   # code here
   data[season].each do |contestant|
     if contestant["status"] = "Winner"
-      return 
+      return contestant["name"].split
 end
 
 def get_contestant_name(data, occupation)
